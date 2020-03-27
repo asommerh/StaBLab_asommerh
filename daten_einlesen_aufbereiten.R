7fe86ddfc92e5e1facb922133faf0923849d5952
Patrick Kaiser <patrickkaiser@Patricks-MacBook-Pro-7.local>
2020-03-27 11:33:14 +0100

    Daten zum Teil eingelesen, noch mit problemen bei package installation (liegt wohl am Heim WLan)

--- daten_einlesen_aufbereiten.R
+++ daten_einlesen_aufbereiten.R
@@ -2,3 +2,25 @@
 ####### Daten einlesen und aufarbeiten ########
 ###############################################
 
+# falls die Packete haven und xlsx nicht installiert sind,
+# werden sie hier installiert
+for (package in c("haven", "xlsx", "dplyr")) {
+  if (!(package %in% dimnames(installed.packages())[[1]])) {
+    install.packages(package)
+  }
+}
+
+library(haven)
+library(xlsx)
+library(dplyr)
+#------------
+# Daten einlesen
+mobile <- read.xlsx("mobileQ_data_example.xlsx")
+t0 <- read_spss("Unipark_t0_example.sav")
+t1 <- read_spss("Unipark_t1_example.sav")
+
+# Versuchspersonencodebenennung vereinheitlichen
+names(t0)[1] <- names(t1)[1] <- "VPCODE"
+fragebogen_daten <- full_join(t0, t1, by = "VPCODE")
+# Warum gibt es in t0 auch eine Variable deren Name bereits auf t1 ended?
+
